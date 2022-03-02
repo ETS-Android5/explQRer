12800c709573a56be53084c4b85ce5b664c5a1c6
Hridyansh <dugar@ualberta.ca>
2022-03-02 05:23:24 -0700

    Created the Comment Class

---
+++ app/src/main/java/com/example/explqrer/Comments.java
@@ -0,0 +1,64 @@
+package com.example.explqrer;
+
+/**
+ * Class to represent the comment
+ */
+public class Comments {
+    private String username;
+    private String comment;
+
+    /**
+     * Constructor to initialize an empty object
+     */
+    public Comments(){
+        username = "";
+        comment = "";
+    }
+
+    /**
+     * Constructor to initialize the object
+     * @param username
+     *  This is the username of the user
+     * @param comment
+     *  This is the comment that the user has left on the QrCode
+     */
+    public Comments(String username ,String comment){
+        this.username = username;
+        this.comment = comment;
+    }
+
+    /**
+     * Function to get the username
+     * @return The username of the user
+     *
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Function to set the username
+     * @param username
+     *  This is the updated username of the user
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Function to get the comment left by the user
+     * @return The comment left by the user
+     */
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     * Function to set the comment
+     * @param comment
+     *  This is the comment to set
+     */
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
--- app/src/main/java/com/example/explqrer/DataHandler.java
+++ app/src/main/java/com/example/explqrer/DataHandler.java
@@ -1,5 +1,18 @@
 package com.example.explqrer;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/**
+ * Class to handle the reading and writing of data
+ */
 public class DataHandler {
+    FirebaseFirestore db;
+    CollectionReference totalQr, pts, unique;
+
+    DataHandler(){
+        db = FirebaseFirestore.getInstance();
+    }
+
 
 }
