2fed77e701cb13cc25f42f47bf2579438427ac8f
Mason <mdbly@ualberta.ca>
2022-03-04 13:13:00 -0700

    Updated shared preferences to store username

--- app/src/main/java/com/example/explqrer/MainActivity.java
+++ app/src/main/java/com/example/explqrer/MainActivity.java
@@ -3,25 +3,62 @@ package com.example.explqrer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
-    BottomNavigationView bottomNavigationView;
+    // DATA
+    public static final String SHARED_PREFS_USERNAME_KEY = "Username";
+    // Data
+    private String username;
+    // Views
+    private BottomNavigationView bottomNavigationView;
+    // Shared Preferences
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        loadData();
+
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnItemSelectedListener(this);
 
+    }
 
+    private void loadData() {
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(SHARED_PREFS_USERNAME_KEY, null);
+        username = gson.fromJson(json, String.class);
+
+        if (username.isEmpty()) {
+            // Random 6 digit number
+            username = String.format(Locale.US,"%06d",
+                    (int) Math.floor(Math.random() * 1000000));
+        }
+    }
+
+    private void saveData() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(username);
+        editor.putString(SHARED_PREFS_USERNAME_KEY, json);
+        editor.apply();
     }
 
     @Override
