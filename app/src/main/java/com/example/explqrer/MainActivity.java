baf6ad8620544bc5a43015c8311bbf6154d8c579
Mason <mdbly@ualberta.ca>
2022-03-03 21:47:39 -0700

    Added bottom nav menu to mainActivity
    Includes updates to related resource files and strings.xml

--- app/src/main/java/com/example/explqrer/MainActivity.java
+++ app/src/main/java/com/example/explqrer/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.explqrer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.map_nav:
+                // TODO: add map activity
+                return true;
+
+            case R.id.profile_nav:
+                // TODO: add profile activity
+                return true;
+
+            case R.id.scan_nav:
+                // TODO: add scan activity
+                return true;
+
+            case R.id.search_nav:
+                // TODO: add search activity
+                return true;
+
+            case R.id.leaderboard_nav:
+                // TODO: add leaderboard activity
+                return true;
+
+        }
+        return false;
     }
 }
