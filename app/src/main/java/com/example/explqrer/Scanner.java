504b6371a573718f76f5b1f250d89704e893aca2
Falak Sethi <falaksethi@Falaks-MacBook-Pro.local>
2022-03-06 02:12:47 -0700

    Added tests for Scanner

--- app/src/main/java/com/example/explqrer/Scanner.java
+++ app/src/main/java/com/example/explqrer/Scanner.java
@@ -17,32 +17,51 @@ public class Scanner {
     public void hashQrCode(){
         this.qrHash = code.getSha256hex();
     }
-
+    /**
+     * Returns the String value of QrHash.
+     *  @return qrHash, a String value.
+     */
     public String getQrHash() {
         return qrHash;
     }
-
+    /**
+     * This set method sets the qrHash.
+     *  @param qrHash a String.
+     */
     public void setQrHash(String qrHash) {
         this.qrHash = qrHash;
     }
-
+    /**
+     * Returns the String value of Code.
+     *  @return code, a String value.
+     */
     public GameCode getCode() {
         return code;
     }
-
+    /**
+     * This set method sets the code.
+     *  @param code a String.
+     */
     public void setCode(GameCode code) {
         this.code = code;
     }
-
+    /**
+     * Returns the String value of Location.
+     *  @return location, a String value.
+     */
     public Location getLocation() {
         return location;
     }
-
+    /**
+     * This set method sets the location.
+     *  @param location a String.
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 }
-// create the qr code obj
+
+// create the qr code object
 // Hash it
 // TODO: Look for existing match
 // TODO: -If it exist add player username
