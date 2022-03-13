23437a1ed47c31cfb4356d240f0e55318d601d02
asadie <asadie@ualberta.ca>
2022-03-13 04:12:27 -0600

    adding the user profile and gallery to dev

--- app/src/main/java/com/example/explqrer/GalleryAdapter.java
+++ app/src/main/java/com/example/explqrer/GalleryAdapter.java
@@ -1,22 +1,33 @@
 package com.example.explqrer;
 
+import static com.example.explqrer.R.id.image;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+/**
+ * This is a class for a custome adapter for the recycler View
+ */
 public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder> {
 
     private ArrayList<ImageListItem> galleryList;
     private Context context;
 
+    /**
+     * Constructor for the class
+     * @param context
+     *   context is where the adapter is being used
+     * @param galleryList
+     *  galleryList is the array list for the adapter
+     */
     public GalleryAdapter(Context context, ArrayList<ImageListItem> galleryList) {
         this.galleryList = galleryList;
         this.context = context;
@@ -29,17 +40,31 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         return new ViewHolder(view);
     }
 
+    /**
+     *
+     * @param viewHolder
+     *      The ViewHolder to hold the
+     * @param i
+     *      The number of items per row
+     */
     @Override
     public void onBindViewHolder(GalleryAdapter.ViewHolder viewHolder, int i) {
         viewHolder.image.setScaleType(ImageView.ScaleType.CENTER_CROP);
         viewHolder.image.setImageResource((galleryList.get(i).getImageId()));
     }
 
+    /**
+     * This gets how many items are in the adapter
+     * @return size of the galleryList of type int
+     */
     @Override
     public int getItemCount() {
         return galleryList.size();
     }
 
+    /**
+     * This class creates the ViewHolder which makes it easier to iterate through images
+     */
     public class ViewHolder extends RecyclerView.ViewHolder{
         private ImageView image;
         public ViewHolder(View view) {
