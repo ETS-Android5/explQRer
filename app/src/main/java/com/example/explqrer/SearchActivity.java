d34ff9832b09dc40dcb0580e119ebe3c365d6a2b
Hridyansh <dugar@ualberta.ca>
2022-03-25 14:45:09 -0600

    Player is able to search other players now

--- app/src/main/java/com/example/explqrer/SearchActivity.java
+++ app/src/main/java/com/example/explqrer/SearchActivity.java
@@ -1,12 +1,16 @@
 package com.example.explqrer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -19,12 +23,27 @@ public class SearchActivity extends AppCompatActivity {
         Button search = (Button) findViewById(R.id.search_Button);
         EditText searchBar = (EditText) findViewById(R.id.search_Bar);
 
+        Context context = this;
+
         // Get the text from the search bar
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String searchText = searchBar.getText().toString();
-
+                new DataHandler().searchPlayers(searchText, new OnSearchPlayersListener() {
+                    @Override
+                    public void searchPlayersListener(ArrayList<String> players) {
+                        ArrayList<ScannedRankLeaderboard> scannedRankLeaderboards = new ArrayList<>();
+                        int rank = 1;
+                        for(String name : players){
+                            String rankString = rank++ +"";
+                            scannedRankLeaderboards.add(new ScannedRankLeaderboard(rankString, name));
+                        }
+                        RecyclerViewAdapterLeaderBoard adapter = new RecyclerViewAdapterLeaderBoard(context, scannedRankLeaderboards);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                    }
+                });
             }
         });
     }
