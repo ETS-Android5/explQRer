5d774efccbd82526a2472856cfe681fefb179746
Hridyansh <dugar@ualberta.ca>
2022-04-01 03:24:25 -0600

    Fixed comments

--- app/src/main/java/com/example/explqrer/UserProfileActivity.java
+++ app/src/main/java/com/example/explqrer/UserProfileActivity.java
@@ -32,9 +32,10 @@ public class UserProfileActivity extends AppCompatActivity implements Navigation
     private static final String[] paths = {"Select to delete QR", "Scan to sign-in", "Edit Profile"};
     private BottomNavigationView bottomNavigationView;
     private PlayerProfile player;
+    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         /* Site: stackoverflow.com
          * Link: https://stackoverflow.com/questions/28460300
          * Author: https://stackoverflow.com/users/3681880/suragch
@@ -46,6 +47,15 @@ public class UserProfileActivity extends AppCompatActivity implements Navigation
          */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        //button for comments
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openComment();
+            }
+        });
 
         // get the player from main activity
         player = MainActivity.getPlayer();
@@ -79,7 +89,7 @@ public class UserProfileActivity extends AppCompatActivity implements Navigation
                                 // Scan to sign in
                                 Intent myIntent = new Intent(getApplicationContext(), ProfileQr.class);
                                 startActivity(myIntent);
-                              break;
+                                break;
                             default:
                                 break;
                         }
@@ -92,8 +102,12 @@ public class UserProfileActivity extends AppCompatActivity implements Navigation
         });
 
         this.populateBanner(player.getName()); //calls populateBanner to put points and scans in in banner recycler view
-//        this.populateGallery(player); //calls populateGallery to put images in the gallery recycler view and provides name of player as parameter
-        GalleryBuilder.populateGallery(player,findViewById(R.id.image_gallery),getApplicationContext());
+        this.populateGallery(player); //calls populateGallery to put images in the gallery recycler view and provides name of player as parameter
+    }
+
+    public void openComment(){
+        Intent intent = new Intent(this, Comment.class);
+        startActivity(intent);
     }
 
 
@@ -199,18 +213,18 @@ public class UserProfileActivity extends AppCompatActivity implements Navigation
      *  Link: https://www.androidauthority.com/how-to-build-an-image-gallery-app-718976/
      *  Author: Adam Sinicki
      */
-//    private void populateGallery(PlayerProfile player){
-//
-//        //set up the RecyclerView for the gallery
-//        RecyclerView galleryRecyclerView = findViewById(R.id.image_gallery);
-//        galleryRecyclerView.setHasFixedSize(true);
-//        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),4);
-//        galleryRecyclerView.setLayoutManager(gridLayoutManager);
-//
-//        ArrayList<GalleryListItem> listOfImages = GalleryList.updateGallery(player);
-//
-//        GalleryAdapter galleryListAdapter = new GalleryAdapter(getApplicationContext(),listOfImages);
-//        System.out.println("before adapter");
-//        galleryRecyclerView.setAdapter(galleryListAdapter);
-//    }
+    private void populateGallery(PlayerProfile player){
+
+        //set up the RecyclerView for the gallery
+        RecyclerView galleryRecyclerView = findViewById(R.id.image_gallery);
+        galleryRecyclerView.setHasFixedSize(true);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getApplicationContext(),4);
+        galleryRecyclerView.setLayoutManager(gridLayoutManager);
+
+        ArrayList<GalleryListItem> listOfImages = GalleryList.updateGallery(player);
+
+        GalleryAdapter galleryListAdapter = new GalleryAdapter(getApplicationContext(),listOfImages);
+        System.out.println("before adapter");
+        galleryRecyclerView.setAdapter(galleryListAdapter);
+    }
 }
