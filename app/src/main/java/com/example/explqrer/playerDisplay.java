08762764c66bd288e18cb44b3fd0a5cd8ac36ee4
Hridyansh <dugar@ualberta.ca>
2022-03-31 22:38:28 -0600

    Player display works

--- app/src/main/java/com/example/explqrer/playerDisplay.java
+++ app/src/main/java/com/example/explqrer/playerDisplay.java
@@ -2,7 +2,9 @@ package com.example.explqrer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class playerDisplay extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class playerDisplay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_display);
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("playerName");
+
+        TextView usernameHolder = findViewById(R.id.usernameHolder);
+        usernameHolder.setText(username);
+        DataHandler dh = DataHandler.getInstance();
+
+        dh.getPlayer(username, new OnGetPlayerListener() {
+            @Override
+            public void getPlayerListener(PlayerProfile player) {
+                usernameHolder.setText(player.getName());
+            }
+        });
     }
 }
